c1f2df6729955cee5956813044eab0891c24218c
K. StaR <jestrella360@hotmail.com>
2023-01-18 13:37:45 -0500

    Add project files.

---
+++ EstrellaS6v2WS/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace EstrellaS6v2WS
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://172.24.96.1/uisrael2023/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<EstrellaS6v2WS.WS.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<EstrellaS6v2WS.WS.Datos> posts = JsonConvert.DeserializeObject<List<EstrellaS6v2WS.WS.Datos>>(content);
+            _post = new ObservableCollection<EstrellaS6v2WS.WS.Datos>(posts);
+
+            MyListView.ItemsSource = _post;
+        }
+
+        private async void btnPost_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PostInsertar());
+        }
+    }
+}
---
+++ EstrellaS6v2WS/PostInsertar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstrellaS6v2WS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostInsertar : ContentPage
+    {
+        public PostInsertar()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client= new WebClient();
+
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                client.UploadValues("http://172.24.96.1/uisrael2023/post.php", "POST", parametros);
+                await DisplayAlert("Alerta", "Ingreso correctamente", "OK");
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Alerta", "ERROR " + ex.Message, "OK");
+            }
+        }
+
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
